5d81bb67ee64d5038795bb049cca6fdf091d0245
dariusstrasel <strasel.darius@gmail.com>
2018-09-03 13:52:34 -0400

    Add prefixLengthNotation method.

--- src/js/lib/LogicalAddressing/SubnetMask.js
+++ src/js/lib/LogicalAddressing/SubnetMask.js
@@ -4,6 +4,10 @@ class SubnetMask extends IpAddress {
     constructor(fourOctetsList) {
         super(fourOctetsList);
     }
+
+    get prefixLengthNotation(){
+        return this.toBinary().split("1").length -1;
+    }
 }
 
 module.exports = SubnetMask;
